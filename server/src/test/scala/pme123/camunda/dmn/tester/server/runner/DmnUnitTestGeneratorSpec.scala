1a89a1b2688285d05309007730a0447571be8e8e
Pascal Mengelt <pascal.mengelt@gmail.com>
2023-02-06 09:57:58 +0100

    Released Version 0.17.0

--- server/src/test/scala/pme123/camunda/dmn/tester/server/runner/DmnUnitTestGeneratorSpec.scala
+++ server/src/test/scala/pme123/camunda/dmn/tester/server/runner/DmnUnitTestGeneratorSpec.scala
@@ -22,184 +22,182 @@ object DmnUnitTestGeneratorSpec extends JUnitRunnableSpec {
           equalTo("EdAfBf_rf6fSuite")
         )
       },
+      test("create Method Name") {
+        assert(generator.methodName(infoRowResult.testInputs))(
+          equalTo("Test Inputs: in1 -> hello Peter! | in2 -> 4.5")
+        )
+      },
+      test("check Test Case successful") {
+        assert(generator.successfulTestCase("INFO"))(
+          containsString("assert(true)") &&
+            containsString("INFO")
+        )
+      },
+      test("create EvalException Test Method") {
+        for {
+          result <- evalExceptionTestMethod
+          _ <- print(s"Result:\n$result")
+        } yield assert(result)(
+          containsString(decisionId) &&
+            containsString("\\n - Failed poorly")
+        )
+      },
+      test("create DmnEvalRowResult Test Method WARN") {
+        for {
+          result <- resultWarnTestMethod
+          _ <- print(s"Result:\n$result")
+        } yield assert(result)(
+          containsString(decisionId) &&
+            containsString("WARN:")
+        )
+      },
+      test("create DmnEvalRowResult Test Method INFO") {
+        for {
+          result <- resultInfoTestMethod
+          _ <- print(s"Result:\n$result")
+        } yield assert(result)(
+          containsString(decisionId) &&
+            containsString("Status: INFO")
+        )
+      },
+      test("create DmnEvalRowResult Test Method INFO with TestCase") {
+        for {
+          result <- resultInfoTestMethodTestCase
+          _ <- print(s"Result:\n$result")
+        } yield assert(result)(
+          containsString(decisionId) &&
+            containsString("Status: INFO")
+        )
+      },
+      test("create Test File") {
+        for {
+          testMethod <- evalExceptionTestMethod
+          testMethod1 <- resultInfoTestMethod
+          testMethod2 <- resultWarnTestMethod
+          testMethod3 <- resultInfoTestMethodTestCase
+          result <- generator.testFile(
+            "MyDecision_12Suite",
+            testMethod,
+            testMethod1,
+            testMethod2,
+            testMethod3
+          )
+          _ <- print(s"Result:\n$result")
+        } yield assert(result)(
+          containsString("package pme123.camunda.dmn.tester.test") &&
+            containsString("class MyDecision_12Suite")
+        )
+      },
+      test("generate Tests") {
+        assertZIO(generator.generate())(isUnit)
+      }
     )
-  /*TODO     test("create Method Name") {
-         assert(generator.methodName(infoRowResult.testInputs))(
-           equalTo("Test Inputs: in1 -> hello Peter! | in2 -> 4.5")
-         )
-       },
-       test("check Test Case successful") {
-         assert(generator.successfulTestCase("INFO"))(
-           containsString("assert(true)") &&
-             containsString("INFO")
-         )
-       },
-       test("create EvalException Test Method") {
-         for {
-           result <- evalExceptionTestMethod
-           _ <- print(s"Result:\n$result")
-         } yield assert(result)(
-           containsString(decisionId) &&
-             containsString("\\n - Failed poorly")
-         )
-       },
-       test("create DmnEvalRowResult Test Method WARN") {
-         for {
-           result <- resultWarnTestMethod
-           _ <- print(s"Result:\n$result")
-         } yield assert(result)(
-           containsString(decisionId) &&
-             containsString("WARN:")
-         )
-       },
-       test("create DmnEvalRowResult Test Method INFO") {
-         for {
-           result <- resultInfoTestMethod
-           _ <- print(s"Result:\n$result")
-         } yield assert(result)(
-           containsString(decisionId) &&
-             containsString("Status: INFO")
-         )
-       },
-       test("create DmnEvalRowResult Test Method INFO with TestCase") {
-         for {
-           result <- resultInfoTestMethodTestCase
-           _ <- print(s"Result:\n$result")
-         } yield assert(result)(
-           containsString(decisionId) &&
-             containsString("Status: INFO")
-         )
-       },
-       test("create Test File") {
-         for {
-           testMethod <- evalExceptionTestMethod
-           testMethod1 <- resultInfoTestMethod
-           testMethod2 <- resultWarnTestMethod
-           testMethod3 <- resultInfoTestMethodTestCase
-           result <- generator.testFile(
-             "MyDecision_12Suite",
-             testMethod,
-             testMethod1,
-             testMethod2,
-             testMethod3
-           )
-           _ <- print(s"Result:\n$result")
-         } yield assert(result)(
-           containsString("package pme123.camunda.dmn.tester.test") &&
-             containsString("class MyDecision_12Suite")
-         )
-       },
-       test("generate Tests") {
-         assertZIO(generator.generate())(isUnit)
-       }
-     )
 
-   private val testCase: TestCase = TestCase(
-     TesterValue.valueMap(infoRowResultTestCase.testInputs),
-     List(
-       dmnTester.TestResult(1, Map("out1" -> "val1", "out2" -> "val2")),
-       dmnTester.TestResult(1, Map("out1" -> "val1", "out2" -> "val3"))
-     )
-   )
-   private lazy val dmnConfig = DmnConfig(
-     decisionId,
-     TesterData(
-       List(
-         TesterInput("in1", nullValue = false, List("hello3", "hello")),
-         TesterInput("in2", nullValue = true, List(3.5, 4))
-       ),
-       List.empty,
-       List(
-         testCase
-       )
-     ),
-     List.empty
-   )
-   private lazy val dmnTables =
-     AllDmnTables(
-       dmnConfig,
-       Seq(
-         DmnTable(
-           decisionId,
-           decisionId,
-           HitPolicy.UNIQUE,
-           None,
-           Seq.empty,
-           Seq.empty,
-           Seq.empty
-         )
-       )
-     )
-   private lazy val dmnResult =
-     DmnEvalResult(
-       dmnTables,
-       Seq("in1", "in2"),
-       Seq("out1", "out2"),
-       Seq.empty,
-       Seq.empty
-     )
+  private val testCase: TestCase = TestCase(
+    TesterValue.valueMap(infoRowResultTestCase.testInputs),
+    List(
+      dmnTester.TestResult(1, Map("out1" -> "val1", "out2" -> "val2")),
+      dmnTester.TestResult(1, Map("out1" -> "val1", "out2" -> "val3"))
+    )
+  )
+  private lazy val dmnConfig = DmnConfig(
+    decisionId,
+    TesterData(
+      List(
+        TesterInput("in1", nullValue = false, List("hello3", "hello")),
+        TesterInput("in2", nullValue = true, List(3.5, 4))
+      ),
+      List.empty,
+      List(
+        testCase
+      )
+    ),
+    List.empty
+  )
+  private lazy val dmnTables =
+    AllDmnTables(
+      dmnConfig,
+      Seq(
+        DmnTable(
+          decisionId,
+          decisionId,
+          HitPolicy.UNIQUE,
+          None,
+          Seq.empty,
+          Seq.empty,
+          Seq.empty
+        )
+      )
+    )
+  private lazy val dmnResult =
+    DmnEvalResult(
+      dmnTables,
+      Seq("in1", "in2"),
+      Seq("out1", "out2"),
+      Seq.empty,
+      Seq.empty
+    )
 
-   private lazy val matchedRule = MatchedRule(
-     "asdfe4",
-     NotTested("1"),
-     Seq("in1" -> "hello3", "in2" -> "4.5"),
-     Seq("out1" -> NotTested("val1"), "out2" -> NotTested("val2"))
-   )
-   private lazy val infoRowResult = DmnEvalRowResult(
-     EvalStatus.INFO,
-     decisionId,
-     Map("in1" -> "hello Peter! how are you.", "in2" -> "4.5"),
-     Seq(
-       matchedRule
-     ),
-     None
-   )
+  private lazy val matchedRule = MatchedRule(
+    "asdfe4",
+    NotTested("1"),
+    Seq("in1" -> "hello3", "in2" -> "4.5"),
+    Seq("out1" -> NotTested("val1"), "out2" -> NotTested("val2"))
+  )
+  private lazy val infoRowResult = DmnEvalRowResult(
+    EvalStatus.INFO,
+    decisionId,
+    Map("in1" -> "hello Peter! how are you.", "in2" -> "4.5"),
+    Seq(
+      matchedRule
+    ),
+    None
+  )
 
-   private lazy val infoRowResultTestCase = DmnEvalRowResult(
-     EvalStatus.INFO,
-     decisionId,
-     Map("in1" -> "hello", "in2" -> "4"),
-     Seq(
-       matchedRule
-     ),
-     None
-   )
+  private lazy val infoRowResultTestCase = DmnEvalRowResult(
+    EvalStatus.INFO,
+    decisionId,
+    Map("in1" -> "hello", "in2" -> "4"),
+    Seq(
+      matchedRule
+    ),
+    None
+  )
 
-   private lazy val warnRowResult = DmnEvalRowResult(
-     EvalStatus.WARN,
-     decisionId,
-     Map("in1" -> "hello", "in2" -> "3"),
-     Seq(
-       matchedRule
-     ),
-     Some(EvalError("This was wrong"))
-   )
+  private lazy val warnRowResult = DmnEvalRowResult(
+    EvalStatus.WARN,
+    decisionId,
+    Map("in1" -> "hello", "in2" -> "3"),
+    Seq(
+      matchedRule
+    ),
+    Some(EvalError("This was wrong"))
+  )
 
-   private lazy val resultWarnTestMethod =
-     generator.testMethod(
-       warnRowResult,
-       dmnResult
-     )
+  private lazy val resultWarnTestMethod =
+    generator.testMethod(
+      warnRowResult,
+      dmnResult
+    )
 
-   private lazy val resultInfoTestMethod = {
-     generator.testMethod(
-       infoRowResult,
-       dmnResult
-     )
-   }
+  private lazy val resultInfoTestMethod = {
+    generator.testMethod(
+      infoRowResult,
+      dmnResult
+    )
+  }
 
-   private lazy val resultInfoTestMethodTestCase = {
-     generator.testMethod(
-       infoRowResultTestCase,
-       dmnResult
-     )
-   }
+  private lazy val resultInfoTestMethodTestCase = {
+    generator.testMethod(
+      infoRowResultTestCase,
+      dmnResult
+    )
+  }
 
-   private def evalExceptionTestMethod = {
-     generator.testMethod(
-       EvalException(dmnConfig, "Failed poorly")
-     )
-   }
+  private def evalExceptionTestMethod = {
+    generator.testMethod(
+      EvalException(dmnConfig, "Failed poorly")
+    )
+  }
 
-  */
 }
